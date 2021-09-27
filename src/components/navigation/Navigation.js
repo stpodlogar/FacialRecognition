b8948374356aff3a301f73908c7549c502c2a136
Stephen Podlogar <stephenpodlogar@Stephens-MacBook-Pro.local>
2021-09-27 15:45:30 -0400

    Remove buttons on signin page

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import faceRec from './faceRecognition.png';
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
   if (isSignedIn) {
     return (
       <nav>
-        <button onClick={() => onRouteChange('signout')} className='btn btn-secondary'>Sign Out</button>
+        <button onClick={() => onRouteChange('signout')} className='btn btn-primary'>Sign Out</button>
       </nav>
     )
   }
   else {
     return (
-      <nav>
-        <button onClick={() => onRouteChange('signin')} className='btn btn-secondary'>Sign In</button>
-        <button onClick={() => onRouteChange('register')} className='btn btn-secondary'>Register</button>
-      </nav>
+      null
     )
   }
 }
