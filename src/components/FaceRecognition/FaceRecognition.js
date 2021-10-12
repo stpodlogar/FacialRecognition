def0990ace4ffd7a231941bc0441f204c0231ed7
Stephen Podlogar <stephenpodlogar@Stephens-MacBook-Pro.local>
2021-10-12 18:07:19 -0400

    Loading spinner

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -4,28 +4,13 @@ import { faImages } from '@fortawesome/free-solid-svg-icons';
 import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageUrl, box }) => {
-  // if (!imageUrl) {
-  //   return (
-  //     <section className='center image-container'>
-  //       <div className='image-wrapper'>
-  //         <img id='image-input' alt='' src={imageUrl} width='100%' height='auto'></img>
-  //         {/* Iterate through imported array box */}
-  //         {box.map((element, index) => {
-  //           return <div className='bounding-box' key={index} style={{top: element.topRow, right: element.rightCol, bottom: element.bottomRow, left: element.leftCol}}></div>
-  //         })}
-  //       </div>
-  //     </section>
-  //     // <div>
-  //     //   There is no image
-  //     // </div>
-  //   )
-  // } else
   return (
     <section className='center image-container'>
       <div className='inner-image-container'>
-        <FontAwesomeIcon icon={faImages} size='5x' inverse />
+        <FontAwesomeIcon icon={faImages} size='5x' inverse className='image-icon'/>
         <div className='image-wrapper'>
           <img id='image-input' alt='' src={imageUrl} width='100%' height='auto'></img>
+          <div id='loading'>LOADING</div>
           {/* Iterate through imported array box */}
           {box.map((element, index) => {
             return <div className='bounding-box' key={index} style={{top: element.topRow, right: element.rightCol, bottom: element.bottomRow, left: element.leftCol}}></div>
@@ -35,5 +20,9 @@ const FaceRecognition = ({ imageUrl, box }) => {
     </section>
   )
 }
+
+const spinner = document.querySelector('#loading');
+const overlay = document.querySelector('.inner-image-container:after');
+console.log(spinner, overlay);
 
 export default FaceRecognition;
